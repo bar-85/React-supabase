d6d3b457ab6d952ca00fded5c11cec62dc6b598b
bar-85 <bajwolskabarbara@gmail.com>
2022-12-19 22:16:49 +0100

    Add header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Create from "./pages/Create";
-import Home from "./pages/Home";
-import Update from "./pages/Update";
-
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Create from './pages/Create'
+import Home from './pages/Home'
+import Update from './pages/Update'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <nav>
-        <h1>Notatnik hodowcy</h1>
-        <Link to='/'>Strona główna</Link>
-        <Link to='/create'>Dodaj nową notatkę</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/:id" element={<Update />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<header>
+        <div className='title'>
+          <h1>Notatnik hodowcy</h1>
+        </div>
+			</header>
+			<nav>
+					<Link to='/'>Strona główna</Link>
+					<Link to='/create'>Dodaj notatkę</Link>
+			</nav>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/create' element={<Create />} />
+				<Route path='/:id' element={<Update />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
