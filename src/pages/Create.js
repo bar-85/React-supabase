7d772a4889dec974312d5736d249a6079f01a560
bar-85 <bajwolskabarbara@gmail.com>
2022-12-22 23:14:06 +0100

    Refresh code

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import supabase from '../config/supabaseClient'
 
 const Create = () => {
-  const navigate = useNavigate()
+	const navigate = useNavigate()
 	const [title, setTitle] = useState('')
 	const [method, setMethod] = useState('')
 	const [formError, setFormError] = useState(null)
@@ -16,32 +16,28 @@ const Create = () => {
 		}
 
 		const { data, error } = await supabase
-    .from('notebook')
-    .insert([{ title, method }])
-    .select()
+		.from('notebook')
+		.insert([{ title, method }])
+		.select()
 
 		if (error) {
-      console.log(error)
-      setFormError('Proszę uzupełnić wszystkie pola')
+			console.log(error)
+			setFormError('Proszę uzupełnić wszystkie pola')
 		}
 		if (data) {
-      console.log(data)
-      setFormError(null)
-      navigate('/')
+			console.log(data)
+			setFormError(null)
+			navigate('/')
 		}
 	}
-
 	return (
 		<div className='page create'>
 			<form onSubmit={handleSubmit}>
 				<label htmlFor='title'>Tytuł:</label>
 				<input type='text' id='title' value={title} onChange={e => setTitle(e.target.value)} />
-
 				<label htmlFor='method'>Opis:</label>
 				<textarea id='method' value={method} onChange={e => setMethod(e.target.value)} />
-
 				<button>Dodaj notatkę</button>
-
 				{formError && <p className='error'>{formError}</p>}
 			</form>
 		</div>
