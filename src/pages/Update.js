3d257a8d37c8e57280e11527a2a11172c7e1b626
bar-85 <bajwolskabarbara@gmail.com>
2022-12-22 23:13:53 +0100

    Add update form to Update.js

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import supabase from '../config/supabaseClient'
 
 const Update = () => {
-  return (
-    <div className='page update'>Aktualizacja</div>
-  )
+	const { id } = useParams()
+	const navigate = useNavigate()
+	const [title, setTitle] = useState('')
+	const [method, setMethod] = useState('')
+	const [formError, setFormError] = useState(null)
+
+	const handleSubmit = async e => {
+		e.preventDefault()
+
+		if (!title || !method) {
+			setFormError('Proszę uzupełnić wszystkie pola')
+      return
+		}
+
+		const { data, error } = await supabase
+    .from('notebook')
+    .update({ title, method })
+    .eq('id', id)
+    .select()
+
+		if (error) {
+			console.log(error)
+			setFormError('Proszę uzupełnić wszystkie pola')
+		}
+		if (data) {
+			console.log(data)
+			setFormError(null)
+			navigate('/')
+		}
+	}
+
+	useEffect(() => {
+		const fetchNotebook = async () => {
+			const { data, error } = await supabase
+      .from('notebook')
+      .select()
+      .eq('id', id)
+      .single()
+
+			if (error) {
+				navigate('/', { replace: true })
+			}
+			if (data) {
+				setTitle(data.title)
+				setMethod(data.method)
+				console.log(data)
+			}
+		}
+		fetchNotebook()
+	}, [id, navigate])
+
+	return (
+		<div className='page update'>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='title'>Tytuł:</label>
+				<input type='text' id='title' value={title} onChange={e => setTitle(e.target.value)} />
+
+				<label htmlFor='method'>Opis:</label>
+				<textarea id='method' value={method} onChange={e => setMethod(e.target.value)} />
+
+				<button>Aktualizacja notatki</button>
+
+				{formError && <p className='error'>{formError}</p>}
+			</form>
+		</div>
+	)
 }
 
 export default Update
