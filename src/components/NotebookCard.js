f4e3384e1e99a704e073b0fd11ff47ec8c308702
bar-85 <bajwolskabarbara@gmail.com>
2022-12-22 23:11:35 +0100

    Add button to NotebookCard

--- src/components/NotebookCard.js
+++ src/components/NotebookCard.js
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom"
+
 const NotebookCard = ({ note }) => {
   return (
     <div className='note-card'>
       <h3>{note.title}</h3>
       <p>{note.method}</p>
+      <div className="buttons">
+        <Link to={'/' + note.id}>
+          <button className="buttonEdit">📝 Edytuj</button>
+        </Link>
+      </div>
     </div>
   )
 }
