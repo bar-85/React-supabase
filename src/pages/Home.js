f5e282572d4129e9e89a68fe4d33e0dd0dd269d6
bar-85 <bajwolskabarbara@gmail.com>
2022-12-23 22:20:53 +0100

    Add orderBy and toggle

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,49 +3,59 @@ import { useEffect, useState } from 'react'
 import NotebookCard from '../components/NotebookCard'
 
 const Home = () => {
-  const [fetchError, setFetchError] = useState(null)
-  const [notebook, setNotebook] = useState(null)
+	const [fetchError, setFetchError] = useState(null)
+	const [notebook, setNotebook] = useState(null)
+	const [orderBy, setOrderBy] = useState('created_at') // created_at - supabase column
+	const [toggle, setToggle] = useState(false)
 
-  const handleDelete = (id) => {
-    setNotebook(prevNotebook => {
-      return prevNotebook.filter(n => n.id !== id)
-    })
-  }
+	const toggleVisibility = () => setToggle(!toggle)
 
-  useEffect(() => {
-    const fetchNotebook = async () => {
-      const { data, error } = await supabase
-      .from('notebook')
-      .select()
+	const handleDelete = id => {
+		setNotebook(prevNotebook => {
+			return prevNotebook.filter(n => n.id !== id)
+		})
+	}
 
-      if (error) {
-        setFetchError('Nie można pobrać notatek')
-        setNotebook(null)
-        console.log(error)
-      }
-      if (data) {
-        setNotebook(data)
-        setFetchError(null)
-      }
-    }
-    fetchNotebook()
-  }, [])
+	useEffect(() => {
+		const fetchNotebook = async () => {
+			const { data, error } = await supabase.from('notebook').select().order(orderBy, { ascending: true })
 
-  return (
-    <div className='page home'>
-      {fetchError && (<p>{fetchError}</p>)}
-      {notebook && (
-        <div className='notebook'>
-          <div className='notebook-grid'>
-          {notebook.map(note => (
-            <NotebookCard key={note.id} note={note} onDelete={handleDelete}/>
-          ))}
-          </div>
-        </div>
-      )}
-    </div>
-  )
+			if (error) {
+				setFetchError('Nie można pobrać notatek')
+				setNotebook(null)
+				console.log(error)
+			}
+			if (data) {
+				setNotebook(data)
+				setFetchError(null)
+			}
+		}
+		fetchNotebook()
+	}, [orderBy])
+
+	return (
+		<div className='page home'>
+			{fetchError && <p>{fetchError}</p>}
+			{notebook && (
+				<div className='notebook'>
+					<div className='order-by'>
+						<button onClick={toggleVisibility}>Sortuj</button>
+						{toggle && (
+							<div className='order-button'>
+								<button onClick={() => setOrderBy('created_at')}>Data</button>
+								<button onClick={() => setOrderBy('title')}>Tytuł</button>
+							</div>
+						)}
+					</div>
+					<div className='notebook-grid'>
+						{notebook.map(note => (
+							<NotebookCard key={note.id} note={note} onDelete={handleDelete} />
+						))}
+					</div>
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default Home
-
